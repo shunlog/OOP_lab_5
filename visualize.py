c637d10c424ac2f988a834ff440900532462f93b
Balan Artiom <artiombalan331@gmail.com>
2022-11-05 22:29:47 +0200

    plot cumulative profits

--- visualize.py
+++ visualize.py
@@ -7,11 +7,25 @@ import json
 with open("out.json") as f:
     h = json.load(f)
 
-for key in h:
-    df = pd.DataFrame(h[key])
-    df['profit'].plot(label=key)
+def regular_plot(h):
+    for key in h:
+        df = pd.DataFrame(h[key])
+        df['profit'].plot(label=key)
 
-plt.xlabel("Days")
-plt.ylabel("Daily profit")
-plt.legend(title="# of cooks")
-plt.show()
+    plt.xlabel("Days")
+    plt.ylabel("Daily profit")
+    plt.legend(title="# of cooks")
+    plt.show()
+
+def cumulative_plot(h):
+    for key in h:
+        df = pd.DataFrame(h[key])
+        df['profit'] = df['profit'].cumsum()
+        df['profit'].plot(label=key)
+    plt.xlabel("Days")
+    plt.ylabel("Cumulative profit")
+    plt.legend(title="# of cooks")
+    plt.show()
+
+regular_plot(h)
+cumulative_plot(h)
