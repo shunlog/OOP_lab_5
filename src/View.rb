cac95aa7d6f9d1c9b16b64f54d8700caf365555f
Balan Artiom <artiombalan331@gmail.com>
2022-12-07 23:55:52 +0200

    import nmtui and try adding 2 windows

--- src/View.rb
+++ src/View.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'ffi-ncurses'
+include FFI::NCurses
+
 class ClockView
   attr_reader :text
   def initialize(model, time=0)
@@ -9,10 +12,6 @@ class ClockView
 
   def update(time)
     @text = "#{time}"
-  end
-
-  def print
-    puts @text
   end
 end
 
@@ -26,21 +25,44 @@ class DateView
   def update(date)
     @text = "#{date}"
   end
-
-  def print
-    puts @text
-  end
 end
 
 
 class TUIView
   def initialize(model)
+    @model = model
+
+    initscr
+    curs_set 0
+    raw
+    noecho
+    keypad stdscr, true
+    scrollok stdscr, true
+
+    @win1 = newwin(7, 20, 4, 15)
+    box(@win1, 0, 0)
+    @win2 = newwin(7, 20, 20, 15)
+    box(@win2, 0, 0)
+
+    @iwin1 = derwin(@win1, 4, 8, 2, 2)
+    @iwin2 = derwin(@win2, 4, 8, 2, 2)
+
+    scrollok @iwin1, true
+    scrollok @iwin2, true
+
     @clock_view = ClockView.new(model)
     @date_view = DateView.new(model)
   end
 
   def print
-    @clock_view.print
-    @date_view.print
+    werase(@iwin1)
+    werase(@iwin2)
+    mvwaddstr(@iwin1, 0, 0, @clock_view.text)
+    mvwaddstr(@iwin2, 0, 0, @date_view.text)
+
+    wrefresh(@win1)
+    wrefresh(@win2)
+    wrefresh(@iwin1)
+    wrefresh(@iwin2)
   end
 end
